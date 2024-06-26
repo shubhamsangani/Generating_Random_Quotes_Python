51290233cc23e5ece3ee8b25f91c37a21ea615e4
SHUBHAM_SANGANI <shubhamsangani5503@gmail.com>
2024-06-26 14:53:56 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+import random
+ 
+# List of random quotes
+quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+    "Stay hungry, stay foolish. - Steve Jobs",
+    "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+    "I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison"
+]
+ 
+# Select and print a random quote
+random_quote = random.choice(quotes)
+print("Random Quote:", random_quote)
